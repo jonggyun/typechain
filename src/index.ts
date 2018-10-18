1138f61155bf70df41dfdb91f8eaea9f21cbb707
JongGyun Park <jonggyuni@gmail.com>
2018-10-18 23:01:49 +0900

    create block by typescript

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,100 @@
-class Human {
-  public name: string;
-  public age: number;
-  public gender: string;
-  constructor(name: string, age: number, gender?: string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+import * as CryptoJS from 'crypto-js';
+
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+
+  static calculateBlockHash = (
+    index: number, 
+    previousHash: string, 
+    timestamp: number, 
+    data: string,
+    ): string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+  static validateStructure = (aBlock: Block): boolean => 
+    typeof aBlock.index === 'number' && 
+    typeof aBlock.hash === 'string' && 
+    typeof aBlock.previousHash === 'string' && 
+    typeof aBlock.timestamp === 'number' &&
+    typeof aBlock.data === 'string';  
+
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ){
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
   }
 }
-// interface와 class의 차이는? 뭐지?? 둘다 사용 가능한듯.
 
-const lynn = new Human("Lynn", 18, "female");
+const genesisBlock: Block = new Block(0, "213123123213", "", "Hello", 123456);
 
-const sayHi = (person: Human): void => { // ?를 통해서 선택적으로 사용할 수 있음.
-  console.log(`Hello! ${person.name} you are ${person.age}, you are a ${person.gender}!!`);
+let blockchain: Block[] = [genesisBlock]; // array는 이렇게 표현한다.
+
+// blockchain.push("stuff"); // typescript에서는 허용이 안됨.
+
+const getBlockchain = (): Block[] => blockchain;
+
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data: string) : Block => {
+  const previousBlock : Block = getLatestBlock();
+  const newIndex : number = previousBlock.index + 1;
+  const newTimestamp : number = getNewTimeStamp();
+  const nextHash: string = Block.calculateBlockHash(
+    newIndex, 
+    previousBlock.hash,
+    newTimestamp,
+    data
+  );
+  const newBlock: Block = new Block(
+    newIndex, 
+    nextHash, 
+    previousBlock.hash, 
+    data, 
+    newTimestamp
+  );
+  addBlock(newBlock);
+  return newBlock;
 }
 
-// const sayHi: (name: any, age: any, gender?: any) => void
-sayHi(lynn);
+const getHashforBlock = (aBlock: Block) :string => Block.calculateBlockHash(aBlock.index, aBlock.previousHash, aBlock.timestamp, aBlock.data);
+
+const isBlockValid = (candidateBlock: Block, previousBlock: Block) : boolean => {
+  if (!Block.validateStructure(candidateBlock)) {
+    return false;
+  } else if (previousBlock.index + 1 !== candidateBlock.index) {
+    return false;
+  } else if (previousBlock.hash !== candidateBlock.previousHash) {
+    return false;
+  } else if (getHashforBlock(candidateBlock) != candidateBlock.hash) {
+    return false;
+  } else {
+    return true;
+  } 
+};
+
+const addBlock = (candidateBlock: Block) : void => {
+  if (isBlockValid(candidateBlock, getLatestBlock())) {
+    blockchain.push(candidateBlock);
+  }
+};
+
+createNewBlock('second block');
+createNewBlock('third block');
+createNewBlock('fourth block');
+
+console.log(blockchain);
 
 export {}; // typescrip 규칙인듯. 이거 없으면 위에 에러남.
