aeec980b5ce13f7e40bf777d381e0ed774cc08af
JongGyun Park <jonggyuni@gmail.com>
2018-10-18 22:17:38 +0900

    typescript 2. create Class for Typecheck, plz compare Interface

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,22 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender?: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
+// interface와 class의 차이는? 뭐지?? 둘다 사용 가능한듯.
 
-const person = {
-  name: 'park',
-  age: 11,
-  gender: 'male'
-}
+const lynn = new Human("Lynn", 18, "female");
 
 const sayHi = (person: Human): void => { // ?를 통해서 선택적으로 사용할 수 있음.
   console.log(`Hello! ${person.name} you are ${person.age}, you are a ${person.gender}!!`);
 }
 
 // const sayHi: (name: any, age: any, gender?: any) => void
-sayHi(person);
+sayHi(lynn);
 
 export {}; // typescrip 규칙인듯. 이거 없으면 위에 에러남.
